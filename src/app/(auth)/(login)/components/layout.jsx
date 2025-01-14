074eb6ce60f6b947ad2d2a8d6f6d9a6f4184e636
digvijaysinh padhiyar <connecttodigvijay@gmail.com>
2025-01-14 23:50:15 +0000

    email login completed..

--- src/app/(auth)/(login)/components/layout.jsx
+++ src/app/(auth)/(login)/components/layout.jsx
@@ -7,10 +7,10 @@ import { EmailSignIn } from "./emailSignIn";
 
 export function LoginLayout({ pageHeading }) {
     return (
-        <main className="border h-screen border-green-600">
-            <div className="container h-[100%] mx-auto flex flex-col items-center justify-center border border-red-600">
+        <main className="h-screen">
+            <div className="container h-[100%] mx-auto flex flex-col items-center justify-center">
                 {/* Centered Content */}
-                <div className="w-[80%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[30%] text-center border">
+                <div className="w-[80%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[30%] text-center">
                     <HeadingTwo text={pageHeading} />
 
                     {/* Social Sign-in options */}
@@ -20,7 +20,7 @@ export function LoginLayout({ pageHeading }) {
                         </div>
                     ))}
 
-                    <div className="my-3 flex flex-row items-center justify-between items-center">
+                    <div className="my-4 flex flex-row items-center justify-between items-center">
                         <Separator className="flex-1" />
                         <p className="mx-3 text-sm font-medium leading-none">or</p>
                         <Separator className="flex-1" />
